0c345597ea8921d0e3fa91481d9f70155fb8c17a
matheus <matheus.tamataya@gmail.com>
2026-04-28 11:46:57 -0300

    feat: Estruturando e DOM

--- src/js/script.js
+++ src/js/script.js
@@ -25,3 +25,21 @@ console.log(texto2)
 // join 0 junta tudo em uma unica string (processo reverso)
 const texto3 = texto2.join(" "); //mudar o caractere entre aspas muda o que aparece entre as palavras
 console.log(texto3)
+
+//METODO toFixed, é pra ajustar as casas decimais
+
+const precoProduto = 199.99
+const desconto = 0.15;
+const precoFinal = precoProduto *(1-desconto);
+console.log(precoFinal);
+console.log(`R$ ${precoFinal.toFixed(2)}`)
+
+// template strings, concatenar com `
+
+
+
+const Titulo=document.getElementById("titulo")
+
+Titulo.innerText= "O DOM MUDOU o TEXO"
+
+Titulo.style.color="blue"
